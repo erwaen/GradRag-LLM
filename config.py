1ac87de91ef4d592b525f2b35eee92753231d394
erwaen <erikwasmosy98@gmail.com>
2025-05-09 13:10:00 -0400

    remove unnecessary files

--- config.py
+++ config.py
@@ -3,18 +3,12 @@ from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    #AZURE_OPENAI_API_KEY: str
     OPENAI_API_KEY: str 
-   # AZURE_OPENAI_ENDPOINT: str
     CSRANKINGS_BASE_URL: str
     STORAGE_DIR: str
-  #  SPIDER_API_KEY: str
     QDRANT_URL: str
     QDRANT_API_KEY: str
     COHERE_API_KEY: str
-    #BING_API_KEY: str
-   # BING_API_ENDPOINT: str
-   # TAVILY_API_KEY: str
     STORAGE_DIR: str = "data"
     model_config = SettingsConfigDict(env_file=".env2", env_file_encoding="utf-8",)
 
