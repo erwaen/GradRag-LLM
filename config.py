e47fc730791c8e1909a9496e5b9f41c6ecff3870
Erik Wasmosy <erikwasmosy98@gmail.com>
2025-09-14 00:27:45 -0400

    change configs

--- config.py
+++ config.py
@@ -4,15 +4,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: str 
+    CSRANKINGS_BASE_URL: str = "https://csrankings.org"
     GEMINI_API_KEY: str
-    CSRANKINGS_BASE_URL: str
-    STORAGE_DIR: str
-    QDRANT_URL: str
-    QDRANT_API_KEY: str
-    COLLECTION_NAME: str # e.g from qdrant
-    # COHERE_API_KEY: str
     STORAGE_DIR: str = "data"
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8",)
+    QDRANT_URL: str = "http://localhost:6333"
+    QDRANT_API_KEY: str = ""
+    COLLECTION_NAME: str = "advisors"
+    COHERE_API_KEY: str = ""
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        extra="ignore"  # Ignora variables extra en el .env
+    )
 
 def get_settings():
     return Settings()
